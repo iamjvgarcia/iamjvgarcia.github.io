e88002ad70637c225afbc45c682d3c4b355f56b9
Jill Villoso Garcia <akirakira013@gmail.com>
2021-05-22 23:59:01 +0800

    preston page continued

---
+++ lesson5/js/prestonjs.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    document.getElementsByClassName("links")[0].classList.toggle("responsive");
+}
+
+let date = new Date();
+let year = date.getFullYear();
+let day = date.getDay();
+let month = date.getMonth()+1;
+let daynumber = date.getDate();
+document.querySelector("#currentyear").textContent = year;
+let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+let nameday = daylist[day];
+let monthlist = ["January", "February", "March", "April" ,"May", "June", "July", "August", "September", "October", "November", "December"]
+let monthname = monthlist[month];
+document.querySelector("#currdate").textContent =  `${nameday}, ${daynumber} ${monthname} ${year}`;
+
