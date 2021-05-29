06603c45ef564203d4e5af97cb06d2c4bd5a2a4e
Jill Villoso Garcia <akirakira013@gmail.com>
2021-05-30 00:36:47 +0800

    Fixed JS

---
+++ lesson5/bomscript.js
@@ -0,0 +1,23 @@
+const fav = document.querySelector('#fav');
+const button = document.querySelector('button');
+const output = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+  if (fav.value != '') {
+    // create the elements in the list
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+  // change some properties ....... textcontent
+  li.textContent = fav.value;
+  deletebutton.textContent = 'X';
+  // ...... add the button to the li
+  li.append(deletebutton);
+  output.append(li);
+  deletebutton.addEventListener('click', function() {
+    output.removeChild(li);
+    fav.focus;
+});
+fav.value - '';
+fav.focus;
+  }
+});
