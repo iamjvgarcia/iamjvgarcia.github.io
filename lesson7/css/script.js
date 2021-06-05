5ff4e1198017f9b39ee4b9c3615c3a905e465abe
Jill Villoso Garcia <akirakira013@gmail.com>
2021-06-06 06:47:01 +0800

    created gallery and edited file locations

---
+++ lesson7/css/script.js
@@ -0,0 +1,27 @@
+//Toggle Menu
+function toggleMenu() {
+    document.getElementsByClassName("links")[0].classList.toggle("responsive");
+}
+
+//Date
+let date = new Date();
+let year = date.getFullYear();
+let day = date.getDay();
+let month = date.getMonth()+1;
+let daynumber = date.getDate();
+document.querySelector("#currentyear").textContent = year;
+let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+let nameday = daylist[day];
+let monthlist = ["January", "February", "March", "April" ,"May", "June", "July", "August", "September", "October", "November", "December"]
+let monthname = monthlist[month];
+document.querySelector("#currdate").textContent =  `${nameday}, ${daynumber} ${monthname} ${year}`;
+
+//pancakes
+const pancakes = document.querySelector(".pancakes");
+const dayOfWeek = date.getDay();
+if (dayOfWeek == 5) {
+  pancakes.style.display = "block";
+} else {
+  pancakes.style.display = "none";
+}
+
