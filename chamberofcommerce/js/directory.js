bafb3f30d9b8549dd4e03c40441619dc666dd766
Jill Villoso Garcia <akirakira013@gmail.com>
2021-07-21 17:43:15 +0800

    test

--- chamberofcommerce/js/directory.js
+++ chamberofcommerce/js/directory.js
@@ -1,5 +1,5 @@
-//source of prophet information in json
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+//test json
+const requestURL = 'https://iamjvgarcia.github.io/chamberofcommerce/json/directory.json';
 
 fetch(requestURL)
     .then(function(response) {
@@ -7,31 +7,73 @@ fetch(requestURL)
     })
     .then(function(jsonObject) {
         
-        const prophets = jsonObject['prophets']
+        const companies = jsonObject['companies']
         
-        for (let i = 0; i < prophets.length; i++){
+        for (let i = 0; i < companies.length; i++){
 
-        let card = document.createElement('section');
+        let drow = document.getElementsByClassName('row');
+        let dcol = document.getElementsByClassName('column');
         let h2 = document.createElement('h2');
-        let bdate = document.createElement('p');
-        let bplace = document.createElement('p');
-        let eximage = document.createElement('img');
+        h2.setAttribute('class','compname')
+        let compadd = document.createElement('p');
+        let cphone = document.createElement('p');
+        let cmobile = document.createElement('p');
+        let compemail = document.createElement('p');
+        let compweb = document.createElement('p');
+        let logoimg = document.createElement('img');
 
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        bdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        bplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-        eximage.setAttribute('src', prophets[i].imageurl);
-        eximage.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+        h2.textContent = companies[i].name ;
+        compadd.textContent = 'Address: ' + companies[i].address;
+        cphone.textContent = 'Phone Number: ' + companies[i].phone;
+        cmobile.textContent = 'Mobile Phone: ' + companies[i].mobile;
+        compemail.textContent = 'Email: ' + companies[i].email;
+        compweb.textContent = 'Website: ' + companies[i].website;
+        logoimg.setAttribute('src', companies[i].logo);
+        logoimg.setAttribute('alt', companies[i].name);
         
-        card.appendChild(h2);
-        card.appendChild(bdate);
-        card.appendChild(bplace);
-        card.appendChild(eximage);
+        dcol.appendChild(h2);
+        dcol.appendChild(compadd);
+        dcol.appendChild(cphone);
+        dcol.appendChild(cmobile);
+        dcol.appendChild(compemail);
+        dcol.appendChild(compweb);
 
-        document.querySelector('div.cards').appendChild(card);
+        document.querySelector('div.dcontainer').appendChild(drow);
     }
     });
 
+    //grid 
+
+ // Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
+
+/* Optional: Add active class to the current button (highlight it) */
+var container = document.getElementById("btnContainer");
+var btns = container.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
 
 
